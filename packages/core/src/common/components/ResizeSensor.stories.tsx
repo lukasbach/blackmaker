d9dae72e2e2ea943b389c61272278419e9cf0ed1
Lukas Bach <lbach@outlook.de>
2021-01-15 18:24:10 +0100

    ResizeSensor

---
+++ packages/core/src/common/components/ResizeSensor.stories.tsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Meta } from '@storybook/react';
+import { ResizeSensor } from './ResizeSensor';
+import cxs from 'cxs';
+
+export default {
+  title: 'Core/Common/Resize Sensor',
+  component: ResizeSensor,
+} as Meta;
+
+export const ResizeSensorExample = () => (
+  <ResizeSensor
+    renderContent={(ref, currentSize) => (
+      <div className={cxs({ width: '100%', height: '100%' })} ref={ref}>
+        Resize the window!<br />
+        {currentSize && `Currently ${currentSize.contentRect.width}x${currentSize.contentRect.height}`}
+      </div>
+    )}
+    onResize={console.log}
+  />
+);
---
+++ packages/core/src/common/components/ResizeSensor.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import ResizeObserverLib from "resize-observer-polyfill";
+import { useEffect, useRef, useState } from 'react';
+
+export interface ResizeSensorProps {
+  renderContent: (ref: React.MutableRefObject<any>, currentSize?: ResizeObserverEntry) => JSX.Element;
+  onResize?: (entries: ResizeObserverEntry[], observer: ResizeObserver) => any;
+}
+
+export const ResizeSensor: React.FC<ResizeSensorProps> = props => {
+  const [currentSize, setCurrentSize] = useState<ResizeObserverEntry>();
+  const ref = useRef<HTMLElement>();
+  const observer = useRef(new ResizeObserverLib((entries, observer) => {
+    props.onResize?.(entries, observer);
+    setCurrentSize(entries[0]);
+  }));
+  useEffect(() => {
+    if (ref.current) {
+      observer.current.observe(ref.current);
+    }
+    return () => observer.current.disconnect();
+  }, [props.renderContent]);
+
+  return props.renderContent(ref, currentSize);
+};
